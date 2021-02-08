2c2ee75d70859006b1a9d126e3620a086631cf3d
Lee424 <tmdghks424@naver.com>
2021-02-09 01:52:23 +0900

    Create Project

---
+++ Unsplash/Unsplash/AppDelegate/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Unsplash
+//
+//  Created by seunghwan Lee on 2021/02/08.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+//        UINavigationBar.appearance().barTintColor = .yellow
+//        UINavigationBar.appearance().tintColor = .gray
+        
+        UINavigationBar.appearance().barStyle = .blackOpaque
+        return true
+        
+
+        return true
+    }
+}
+

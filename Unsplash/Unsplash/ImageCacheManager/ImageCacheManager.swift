b210d6004397ab4b1c99f708e622dd028fa46b27
Lee424 <tmdghks424@naver.com>
2021-02-15 09:28:16 +0900

    [Feat] ImageCacheManager

---
+++ Unsplash/Unsplash/ImageCacheManager/ImageCacheManager.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCacheManager.swift
+//  Unsplash
+//
+//  Created by seunghwan Lee on 2021/02/15.
+//
+
+import Foundation
+
+class ImageCacheManager {
+    static let shared = ImageCacheManager()
+    
+    private init() {}
+    
+    let cachedImages = NSCache<NSString, NSData>()
+}

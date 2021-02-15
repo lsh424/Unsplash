a905265226be0ee16172deb02cc83c277f06ca81
Lee424 <tmdghks424@naver.com>
2021-02-15 09:27:50 +0900

    [Add] Models

--- Unsplash/Unsplash/Models/Photo.swift
+++ Unsplash/Unsplash/Models/Photo.swift
@@ -7,6 +7,79 @@
 
 import Foundation
 
+class PhotoObject {
+    var nextPage: Int = 1
+    var photos: [Photo] = []
+    private lazy var totalPage: Int = 1
+    
+    func fetchPhotos(completion: @escaping () -> Void) {
+        NetworkManager.shared.fetchPhotos { [weak self] (newPhotos) in
+            self?.photos.append(contentsOf: newPhotos)
+            completion()
+        }
+    }
+    
+    func updatePhotos(completion: @escaping ([IndexPath]) -> Void) {
+        NetworkManager.shared.fetchPhotos(page: nextPage) { [weak self] (newPhotos) in
+            
+            guard let strongSelf = self else {return}
+            self?.photos.append(contentsOf: newPhotos)
+            
+            let newPhotosCount = newPhotos.count
+            let startIndex = (strongSelf.photos.count) - newPhotosCount
+            let endIndex = startIndex + newPhotosCount
+            var indexPaths = [IndexPath]()
+            
+            for index in startIndex..<endIndex {
+                indexPaths.append(IndexPath(item: index, section: 0))
+            }
+            completion(indexPaths)
+        }
+    }
+    
+    func searchPhotos(with search: String, completion: @escaping () -> Void) {
+        NetworkManager.shared.searchPhotos(query: search) { [weak self] (searchResult) in
+            let newPhotos = searchResult.photos
+            self?.photos.append(contentsOf: newPhotos)
+            self?.totalPage = searchResult.totalPages
+            completion()
+        }
+    }
+    
+    func updateSearchPhotos(with search: String, completion: @escaping ([IndexPath]) -> Void) {
+        guard nextPage <= totalPage else {return}
+        
+        NetworkManager.shared.searchPhotos(query: search, page: nextPage) { [weak self] (searchResult) in
+            
+            guard let strongSelf = self else {return}
+            
+            let newPhotos = searchResult.photos
+            
+            self?.photos.append(contentsOf: newPhotos)
+            
+            let newPhotosCount = newPhotos.count
+            let startIndex = (strongSelf.photos.count) - newPhotosCount
+            let endIndex = startIndex + newPhotosCount
+            var indexPaths = [IndexPath]()
+            
+            for index in startIndex..<endIndex {
+                indexPaths.append(IndexPath(item: index, section: 0))
+            }
+            completion(indexPaths)
+        }
+    }
+}
+
+struct SearchResult: Codable {
+    let totalPages: Int
+    let photos: [Photo]
+    
+    enum CodingKeys: String, CodingKey {
+        case totalPages = "total_pages"
+        case photos = "results"
+    }
+}
+
 struct Photo: Codable {
     let id: String
     let user: User
--- Unsplash/Unsplash/Models/PhotoInfo.swift
+++ Unsplash/Unsplash/Models/PhotoInfo.swift
@@ -7,4 +7,35 @@
 
 import Foundation
 
+struct PhotoInfo: Codable {
+    let width: Int
+    let height: Int
+    let createdAt: Date
+    let exif: Exif
+    
+    enum CodingKeys: String, CodingKey {
+        case width
+        case height
+        case createdAt = "created_at"
+        case exif
+    }
+}
+
+struct Exif: Codable {
+    let make: String?
+    let model: String?
+    let exposureTime: String?
+    let aperture: String?
+    let focalLength: String?
+    let iso: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case make
+        case model
+        case exposureTime = "exposure_time"
+        case aperture
+        case focalLength = "focal_length"
+        case iso
+    }
+}
 

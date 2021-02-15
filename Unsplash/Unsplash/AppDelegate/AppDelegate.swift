d59c9e53fda61381d2d2a4f49a4cd384f27f6a6b
Lee424 <tmdghks424@naver.com>
2021-02-15 09:23:56 +0900

    불필요 코드 제거, 프로젝트 상태 업데이트

--- Unsplash/Unsplash/AppDelegate/AppDelegate.swift
+++ Unsplash/Unsplash/AppDelegate/AppDelegate.swift
@@ -9,21 +9,9 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-//        UINavigationBar.appearance().barTintColor = .yellow
-//        UINavigationBar.appearance().tintColor = .gray
-        
-        UINavigationBar.appearance().barStyle = .blackOpaque
-        
-        
-        
-        NetworkMonitor.shared.startMonitoring()
-        
-
         return true
     }
 }
